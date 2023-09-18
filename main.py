afca9efae69c46beda5625c67f0b2fe16b129208
Eda Rupçalı <120987688+rupcali@users.noreply.github.com>
2023-09-18 13:05:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,7 @@ while len(guessed_states) != 50:
     answer_state = (screen.textinput(title=f"{correct_guess}/50 States Correct", prompt="What's another state's name?")).title()
     print(answer_state)
     if answer_state == "Exit":
-        missing_states = []
-        for state in state_list:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in states_list if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
@@ -33,5 +30,3 @@ while len(guessed_states) != 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
-        
-
